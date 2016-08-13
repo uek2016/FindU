8b18b634a1254c3322b54650ae9b636840da75a4
nate-river <mayanlong7@icloud.com>
2016-08-13 17:40:31 +0800

    ....

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: 2016-08-06 02:24:44
+-- Generation Time: 2016-08-13 11:29:16
 -- 服务器版本： 5.5.42
 -- PHP Version: 7.0.0
 
@@ -15,6 +15,27 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `uek` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `uek`;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `uek_extra_work`
+--
+
+DROP TABLE IF EXISTS `uek_extra_work`;
+CREATE TABLE `uek_extra_work` (
+  `wid` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `w_title` varchar(255) NOT NULL,
+  `w_keywords` varchar(255) NOT NULL,
+  `w_progress` int(11) NOT NULL,
+  `w_start_time` int(11) NOT NULL,
+  `w_end_time` int(11) NOT NULL,
+  `ctime` int(11) NOT NULL,
+  `mtime` int(11) NOT NULL,
+  `w_date` int(11) NOT NULL,
+  `is_del` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -35,11 +56,17 @@ CREATE TABLE `user` (
   `ctime` int(11) NOT NULL,
   `mtime` int(11) NOT NULL,
   `is_del` int(11) NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=59 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `uek_extra_work`
+--
+ALTER TABLE `uek_extra_work`
+  ADD PRIMARY KEY (`wid`);
 
 --
 -- Indexes for table `user`
@@ -52,7 +79,12 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `uek_extra_work`
+--
+ALTER TABLE `uek_extra_work`
+  MODIFY `wid` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=59;
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT;
