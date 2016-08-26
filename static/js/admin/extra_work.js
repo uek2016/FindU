1e3d1b4e9408eaeb740bde242fd94d6c04c6df9c
mszsnn <307585280>
2016-08-26 19:23:16 +0800

    最终版本，更改完毕

--- static/js/admin/extra_work.js
+++ static/js/admin/extra_work.js
@@ -3,7 +3,7 @@ $(function(){
 //	当前月的第一天	
 	var color=["#FFF4AC","#C4F9CD","#F6E2F7","#FDCCD1","#FFD8BE","#CBEBF8","#FBDFD2","#FED1E1","#B5FFA4"];
 	var colorfont=["#624920","#23672E","#5C3461","#89353D","#814218","#164D62","#49362D","#801B3F","#336419"];
-	
+	var xingqi={0:"周日",1:"周一",2:"周二",3:"周三",4:"周四",5:"周五",6:"周六"};
 	function setMOnth(month){
 		//  即这个月从1日到最后一天
 			var date = moment('2016-'+month+'-1', 'YYYY-MM-DD');
@@ -17,11 +17,13 @@ $(function(){
 			return dates;
 	}
 //	获取到7月所有的天数
-	var allday=setMOnth(7);
-		
+	var nowmonth=moment().month();
+	var allday=setMOnth(6+1);
+//	这是每一个人都数组总额
+	var everyperson=[];	
 	
 	
-	API.getExByMonth(7).then(function(data){
+	API.getExByMonth(6+1).then(function(data){
 	//		data为所有的数据
 		var name=[];
 		//表格中的行  即所有人的姓名
@@ -36,10 +38,10 @@ $(function(){
 			});
 	
 	//	根据名字个数来设置高度
-		var windowheight=$(window).height();
+		var windowheight=$(window).height()-50;
 		var liheight=windowheight/(names.length+1);
 		if(liheight<=40){
-				liheight=40;
+			liheight=40;
 		}
 		$("body").css("line-height",(liheight-2)+"px");
 			
@@ -47,14 +49,16 @@ $(function(){
 		
 	// 	画左上角月份
 		var li=$("<li>");
-		li.html(7);
+		li.html("姓名 / "+(nowmonth+1)+"月");
 		li.css({height:liheight,"line-height":liheight+"px"});
 		li.appendTo($("#name ul"));	
 	
 	
 	//	画时间总计
 		var li=$("<li>");
+		li.attr("id","totle-time");
 		li.html("总计");
+		
 		li.css({height:liheight,"line-height":liheight+"px"});
 		li.appendTo($("#totle ul"));
 		
@@ -66,10 +70,16 @@ $(function(){
 				li.appendTo($("#name ul"));
 		})			
 	
-//	画天数
-		var xingqi={0:"周日",1:"周一",2:"周二",3:"周三",4:"周四",5:"周五",6:"周六"};
+          
+		//	画天数
+		
 		var boxheight=$("#table-parent").width()/7;
-		$("#table").css("width",allday.length*boxheight);
+		
+		var divobj=$("<div>");
+		divobj.attr({"class":"list","month":moment().month()});
+		divobj.css("width",allday.length*boxheight+0.5);
+		
+			
 		var tr=$("<ul>");
 		$.each(allday,function(j,obj){
 				var li=$("<li>");
@@ -77,14 +87,11 @@ $(function(){
 				li.html(moment(obj.value).date()+"号("+xingqi[moment(obj.value).day()]+")");
 				li.appendTo(tr);
 		})
-		tr.appendTo($("#table"));	
+		tr.appendTo(divobj);	
 		
 		
-		
-		
-		
-	//获取每个人在30天的加班具体详情
-				var everyperson=[];
+		//获取每个人在当月天的加班具体详情
+				
 				names.forEach(function(name) {
 							//第一个为当前日期
 				var row = [];
@@ -121,12 +128,14 @@ $(function(){
 					var timedur=obj.end-obj.start;
 					var xiaoshi=(parseInt(timedur/(1000*60))/60).toFixed(1);
 					var str=d.hours()+":"+d.minute()+"-"+d2.hours()+":"+d2.minute()+" "+xiaoshi+"小时";	
-					li.attr("id",obj.wid);
+					li.attr({"id":obj.wid,time:xiaoshi});					
 					li.html("<div class='extra-more'><h1>"+str+"</h1><h2>"+obj.title+"</h2></div><div class='ms-del'></div>");
 					var ms_nub=Math.floor(Math.random()*color.length);
 					if(obj.is_del=="1"){
+						li.attr("is_del",1)
 						li.css({"background-color":"#cccccc",color:colorfont[ms_nub]})
 					}else{
+						li.attr("is_del",0)
 						li.css({"background-color":color[ms_nub],color:colorfont[ms_nub]})
 					}
 					
@@ -135,13 +144,36 @@ $(function(){
 				}
 				li.appendTo(tr);
 			})
-			tr.appendTo($("#table"))
-		})	
+			tr.appendTo(divobj)
+		})
+		divobj.appendTo($("#table"));
+		
+		var listlength=$(".list").length;
+		$("#table").css("width",$(".list").width()*listlength);
+		
+		
+		
+		
+		
+		
 	
 	$("#table-parent").delegate("div:has('.ms-del')", "click", function(event){
-		API.deleteExWorkByWid($(event.target).parent("li").attr("id")).then(function(data){		
+		API.deleteExWorkByWid($(event.target).parent("li").attr("id")).then(function(data){	
 			if(data){
+				if($(event.target).parent("li").attr("is_del")=="0"){
+//					有问题
+					
+					var deltime=$(event.target).parent("li").attr("time");
+					
+					var index=$(".list ul").index($(event.target).parent().parent());
+//					完了之后开始减就ok
+				var nowobj=$("#totle ul li").eq(index);
+					nowobj.html((parseFloat(nowobj.attr("everytime"))-parseFloat(deltime)).toFixed(1)+"小时");
+					nowobj.attr("everytime",(parseFloat(nowobj.attr("everytime"))-parseFloat(deltime)).toFixed(1))					
+				}
+				$(event.target).parent("li").attr("is_del",1);
 				$(event.target).parent("li").css("background-color","#CCCCCC");
+					 	
 			}
 		})	
 	});
@@ -149,36 +181,47 @@ $(function(){
 	
 	
 //	获取总时间，需要每个人的数据
-			var totle_time=[];
-			$.each(everyperson,function(i,val){
-			var time=0;
-			$.each(val,function(j,obj){
-					if(typeof obj=="object"){
-						var timedur=obj.end-obj.start;						
-						var fenzhong=parseInt(timedur/(1000*60));
-						time+=fenzhong;
-					}
+			var totle_time=get_totle_time();
+			console.log(totle_time)
+			function get_totle_time(){
+				var totle_time=[];
+				$.each(everyperson,function(i,val){
+				var time=0;
+				$.each(val,function(j,obj){
+						if(typeof obj=="object"){
+							if(obj.is_del=="0"){
+								var timedur=obj.end-obj.start;						
+								var fenzhong=parseInt(timedur/(1000*60));
+								time+=fenzhong;
+							}							
+						}
+					})
+						time=time/60;
+						time=time.toFixed(1);
+						totle_time.push(time);
 				})
-					time=time/60;
-					time=time.toFixed(1);
-					totle_time.push(time);
-			})
+				return totle_time;
+			}	
 				
 //画总时间
 		$.each(totle_time,function(i,val){
 			var li=$("<li>");
 			li.html(val+"小时");
+			li.attr("everytime",val);
 			li.css({height:liheight,"line-height":liheight+"px"});
 			li.appendTo($("#totle ul"));
 		})
 		
-	
-	
-	
-	
-	
-	
-	
+		
+// 画相对应的width和高度，因为是定位		
+		var $nameheight=$("#name").height();
+		$("#table-parent").css("height",$nameheight);
+		$("#table").css("height",$nameheight);
+		
+//		生成表格
+		$("#check").click(function(){
+			location.href="/api/exwork/excel?m="+(moment().month()+1);
+		})
 					
     })
 	
